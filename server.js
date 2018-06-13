56387d9779dd7a89ec3dbc5185bf2232cbd37236
pichuleperro <39389673+pichuleperro@users.noreply.github.com>
2018-06-12 20:38:01 -0400

    primer commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const colors = require('colors');
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+// express HBS engine 
+hbs.registerPartials(__dirname + '/views/parcials');
+
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: 'Chaparra',
+        anio: new Date().getFullYear()
+    });
+});
+
+let puerto = process.env.PORT || 8080;
+
+app.listen(puerto, () => {
+    console.log(`Escuchando puerto`.blue + ` ${puerto}`.red);
+});
